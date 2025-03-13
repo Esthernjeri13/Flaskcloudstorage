4a4c9839a1b3dde9a238aeeb72a829597b86f624
Esther Mungai <msmungai00@gmail.com>
2025-03-13 22:35:24 +0300

    Fixed routing & static file issues

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def delete(filename):
 if __name__ == '__main__':
     app.run(debug=True)
 
-app = Flask(__name__, static_folder="static")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
